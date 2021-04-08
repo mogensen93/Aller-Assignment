b04e72f96425a5529321ac4df37d8b89c7194ac5
Stationær <rasmusmogensen2@hotmail.com>
2021-04-08 11:10:53 +0200

    Setup header and navigation with links @ mobile res, not responsive yet

---
+++ components/Header/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="w-auto bg-gray-100 sm:mx-h-20 flex justify-between p-2 filter drop-shadow-md">
+            <img src="https://via.placeholder.com/40x50" alt="">
+            <div class="sm:w-10" v-on:click="isNavOpen = !isNavOpen"> 
+                <svg class="block h-auto w-10 pt-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+            </div>
+        </div>
+        <div class="bg-gray-100 pl-12 pr-12 pt-6 w-auto" v-show="isNavOpen">
+            <HeaderSearch />
+            <HeaderNavigationLinks />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                isNavOpen: false
+            }
+        }
+    }
+</script>
+
+<style>
+    .background-color {
+        background-color: #F2EEE4;
+    }
+
+</style>
---
+++ components/Header/NavigationLinks.vue
@@ -0,0 +1,53 @@
+<template>
+<!-- TODO: Lav seperat component, og loop igennem links-->
+<!-- EXTRA: Indsæt den rigtige text -->
+    <div>
+        <div class="grid grid-cols-2 gap-2 ">
+            <div class="leading-loose m-2">
+                <h2 class="text-xl lg:text-lg xl:text-xl">LIV</h2>
+                <nuxt-link to="/" class="block">Selvudvikling</nuxt-link>
+                <nuxt-link to="/" class="block">Kærlighed og sex</nuxt-link>
+                <nuxt-link to="/" class="block">Familieliv</nuxt-link>
+                <nuxt-link to="/" class="block">Brevkasse</nuxt-link>
+                <nuxt-link to="/" class="block">Sundhed</nuxt-link>
+                <nuxt-link to="/" class="block">Gravid</nuxt-link>
+            </div>
+            <div class="leading-loose m-2">
+                <h2 class="text-4xl">KULTUR</h2>
+                <nuxt-link to="/" class="block">Selvudvikling</nuxt-link>
+                <nuxt-link to="/" class="block">Kærlighed og sex</nuxt-link>
+                <nuxt-link to="/" class="block">Familieliv</nuxt-link>
+                <nuxt-link to="/" class="block">Brevkasse</nuxt-link>
+                <nuxt-link to="/" class="block">Sundhed</nuxt-link>
+            </div>
+            <div class="leading-loose m-2">
+                <h2 class="text-4xl">STIL</h2>
+                <nuxt-link to="/" class="block">Selvudvikling</nuxt-link>
+                <nuxt-link to="/" class="block">Kærlighed og sex</nuxt-link>
+                <nuxt-link to="/" class="block">Familieliv</nuxt-link>
+            </div>
+            <div class="leading-loose m-2">
+                <h2 class="text-4xl">AGENDA</h2>
+                <nuxt-link to="/" class="block">Selvudvikling</nuxt-link>
+                <nuxt-link to="/" class="block">Kærlighed og sex</nuxt-link>
+                <nuxt-link to="/" class="block">Familieliv</nuxt-link>
+            </div>
+        </div>
+        <div class="leading-loose m-2">
+             <h2 class="text-4xl">MERE</h2>
+            <div class="grid grid-cols-2 gap-2">
+                <div>
+                    <nuxt-link to="/" class="block">Video</nuxt-link>
+                    <nuxt-link to="/" class="block">Podcast</nuxt-link>
+                    <nuxt-link to="/" class="block">Pling</nuxt-link>
+                    <nuxt-link to="/" class="block">Krydsord</nuxt-link>
+                </div>
+                <div>
+                    <nuxt-link to="/" class="block">Abonnement</nuxt-link>
+                    <nuxt-link to="/" class="block">Kontakt</nuxt-link>
+                    <nuxt-link to="/" class="block">Nyhedsbrev</nuxt-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
