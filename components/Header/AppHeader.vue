19c4e02f2147f14458ebc934db0af5df93588a00
Stationær <rasmusmogensen2@hotmail.com>
2021-04-08 14:56:47 +0200

    Reworking grid in order to fit desktop size inside it aswell.

--- components/Header/AppHeader.vue
+++ components/Header/AppHeader.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
-        <div class="w-auto bg-gray-100 sm:mx-h-20 flex justify-between p-2 filter shadow-lg z-10">
+        <div class="w-auto bg-gray-100 sm:mx-h-20 md:-h-16 flex justify-between md:justify-around filter p-1 shadow-lg z-10">
             <img src="https://via.placeholder.com/40x50" alt="">
-            <div class="sm:w-10" v-on:click="isNavOpen = !isNavOpen"> 
-                <svg class="block h-auto w-10 pt-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <div class="w-auto flex"> 
+                <HeaderSearchDesktop v-show="!isMobile" />
+                <svg class="block h-auto w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" v-on:click="isNavOpen = !isNavOpen">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
+                
+             
             </div>
         </div>
         <div class="w-auto navWindowHeight" v-show="isNavOpen">
-            <HeaderSearch />
-            <HeaderNavigationLinks />
+            <HeaderSearchMobile v-show="isMobile" />
+            <HeaderNavigationLinks @toggleNavigation="toggleNavigation"/>
         </div>
     </div>
 </template>
@@ -19,17 +22,30 @@
     export default {
         data() {
             return{
-                isNavOpen: false
+                isNavOpen: false,
+                isMobile: false
             }
-        }
+        },
+        mounted () {
+            window.addEventListener('resize', this.onResize)
+        },
+        methods: {
+            onResize () {
+                if(this.isMobile = window.innerWidth < 768){
+                    this.isMobile = true;
+                }
+                console.log(this.isMobile);
+            },
+            toggleNavigation(){
+                this.isNavOpen = !this.isNavOpen
+                console.log(this.isNavOpen);
+            }
+
+        },
     }
 </script>
 
-<style>
-    .background-color {
-        background-color: #F2EEE4;
-    }
-
+<style>                    
     .navWindowHeight {
         height: 92vh;
     }
--- components/Header/NavigationLinks.vue
+++ components/Header/NavigationLinks.vue
@@ -1,10 +1,12 @@
 <template>
 <!-- TODO: Lav seperat component, og loop igennem links-->
 <!-- EXTRA: Indsæt den rigtige text -->
-    <div>
-        <div class="grid grid-cols-2 gap-2 bg-gray-100 pl-10 pr-10">
-            <div class="leading-loose m-2">
-                <h2 class="text-3xl md:text-4xl ">LIV</h2>
+<!-- Fix nederste potion af grid -->
+    <div class="md:absolute md:z-50 md:top-0 w-full ">
+        <div class="grid grid-cols-2 gap-2 md:grid-cols-6 md:gap-6 bg-gray-100 px-64 md:pt-9">
+            <img src="https://via.placeholder.com/80x125" class="hidden md:block " alt="">
+            <div class="leading-loose m-2 md:m-0">
+                <h2 class="text-3xl  ">LIV</h2>
                 <nuxt-link to="/" class="block">Selvudvikling</nuxt-link>
                 <nuxt-link to="/" class="block">Kærlighed og sex</nuxt-link>
                 <nuxt-link to="/" class="block">Familieliv</nuxt-link>
@@ -12,42 +14,51 @@
                 <nuxt-link to="/" class="block">Sundhed</nuxt-link>
                 <nuxt-link to="/" class="block">Gravid</nuxt-link>
             </div>
-            <div class="leading-loose m-2">
-                <h2 class="text-3xl md:text-4xl">KULTUR</h2>
+            <div class="leading-loose m-2 md:m-0">
+                <h2 class="text-3xl ">KULTUR</h2>
                 <nuxt-link to="/" class="block">Selvudvikling</nuxt-link>
                 <nuxt-link to="/" class="block">Kærlighed og sex</nuxt-link>
                 <nuxt-link to="/" class="block">Familieliv</nuxt-link>
                 <nuxt-link to="/" class="block">Brevkasse</nuxt-link>
                 <nuxt-link to="/" class="block">Sundhed</nuxt-link>
             </div>
-            <div class="leading-loose m-2">
-                <h2 class="text-3xl md:text-4xl">STIL</h2>
+            <div class="leading-loose m-2 md:m-0">
+                <h2 class="text-3xl ">STIL</h2>
                 <nuxt-link to="/" class="block">Selvudvikling</nuxt-link>
                 <nuxt-link to="/" class="block">Kærlighed og sex</nuxt-link>
                 <nuxt-link to="/" class="block">Familieliv</nuxt-link>
             </div>
-            <div class="leading-loose m-2">
-                <h2 class="text-3xl md:text-4xl">AGENDA</h2>
+            <div class="leading-loose m-2 md:m-0 ">
+                <h2 class="text-3xl ">AGENDA</h2>
                 <nuxt-link to="/" class="block">Selvudvikling</nuxt-link>
                 <nuxt-link to="/" class="block">Kærlighed og sex</nuxt-link>
                 <nuxt-link to="/" class="block">Familieliv</nuxt-link>
+            </div>
+            <div class="w-100 ">
+                <img src="~/assets/img/icons/close.png" class="hidden md:block h-12 w-auto object-contain" v-on:click="toggleNav"/>
             </div>
         </div>
-        <div class="leading-loose m-2 pl-10 pr-10">
-             <h2 class="text-3xl md:text-4xl">MERE</h2>
-            <div class="grid grid-cols-2 gap-2">
-                <div>
-                    <nuxt-link to="/" class="block">Video</nuxt-link>
-                    <nuxt-link to="/" class="block">Podcast</nuxt-link>
-                    <nuxt-link to="/" class="block">Pling</nuxt-link>
-                    <nuxt-link to="/" class="block">Krydsord</nuxt-link>
-                </div>
-                <div>
-                    <nuxt-link to="/" class="block">Abonnement</nuxt-link>
-                    <nuxt-link to="/" class="block">Kontakt</nuxt-link>
-                    <nuxt-link to="/" class="block">Nyhedsbrev</nuxt-link>
-                </div>
-            </div>
+        <div class="leading-loose m-2 px-64 pr-10 flex">
+            <h2 class="text-3xl">MERE</h2>
+            <ul class="grid grid-cols-4">
+                <nuxt-link to="/" class="block">Video</nuxt-link>
+                <nuxt-link to="/" class="block">Podcast</nuxt-link>
+                <nuxt-link to="/" class="block">Pling</nuxt-link>
+                <nuxt-link to="/" class="block">Krydsord</nuxt-link>
+                <nuxt-link to="/" class="block">Abonnement</nuxt-link>
+                <nuxt-link to="/" class="block">Kontakt</nuxt-link>
+                <nuxt-link to="/" class="block">Nyhedsbrev</nuxt-link>
+            </ul>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        methods: {
+            toggleNav(){
+                this.$emit('toggleNavigation');
+            }
+        }
+    }
+</script>
